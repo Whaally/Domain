19f49634d79c7b976adb904e7a61bca323e045dc
Corstian Boerman <mail@corstianboerman.com>
2024-05-29 12:32:43 +0200

    Fix output configuration

--- samples/Skyhop.Domain.Benchmark/Program.cs
+++ samples/Skyhop.Domain.Benchmark/Program.cs
@@ -1,6 +1,9 @@
+using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Running;
 
 BenchmarkSwitcher
     .FromAssembly(typeof(Program).Assembly)
-    .Run(args);
+    .Run(args, config: ManualConfig
+        .Create(DefaultConfig.Instance)
+        .WithOptions(ConfigOptions.DisableOptimizationsValidator)); // Must be done because FluentResults is not optimized
     
