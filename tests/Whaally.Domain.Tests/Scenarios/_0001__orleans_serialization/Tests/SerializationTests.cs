b03a7ba7ad752a8858d31cd9820e82d53628020a
Corstian Boerman <mail@corstianboerman.com>
2024-11-20 14:53:48 +0100

    Add an explanation for skipping tests

--- tests/Whaally.Domain.Tests/Scenarios/_0001__orleans_serialization/Tests/SerializationTests.cs
+++ tests/Whaally.Domain.Tests/Scenarios/_0001__orleans_serialization/Tests/SerializationTests.cs
@@ -12,23 +12,25 @@ public class SerializationTests(ClusterFixture fixture)
         => new DomainContext(fixture.Cluster.Client.ServiceProvider)
             .EvaluateCommand(Guid.NewGuid().ToString(), new TestCommand());
 
-    [Fact(Skip = "_")]
+    private const string _skipReason = "Fails when running on GitHub Actions";
+    
+    [Fact(Skip = _skipReason)]
     public async Task CanInvokeCommand()
         => (await _evaluation).IsSuccess.Should().BeTrue();
 
-    [Fact(Skip = "_")]
+    [Fact(Skip = _skipReason)]
     public async Task ReceivesSerializedEventInResponse()
         => (await _evaluation).Value.Should().ContainSingle();
 
-    [Fact(Skip = "_")]
+    [Fact(Skip = _skipReason)]
     public async Task CanInspectEvent()
         => (await _evaluation).Value[0].Should().BeAssignableTo<IEventEnvelope<TestEvent>>();
 
-    [Fact(Skip = "_")]
+    [Fact(Skip = _skipReason)]
     public async Task CanAccessEventData()
         => (await _evaluation).Value[0].Message.Should().BeAssignableTo<TestEvent>();
 
-    [Fact(Skip = "_")]
+    [Fact(Skip = _skipReason)]
     public async Task FlagIsTrue()
         => ((TestEvent)(await _evaluation).Value[0].Message).Flag.Should().BeTrue();
 }
